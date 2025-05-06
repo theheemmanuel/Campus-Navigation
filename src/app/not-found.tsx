b97b97808e3ba0cadbac569c7294dba038ca8e10
theheemmanuel <oladirane@gmail.com>
2025-05-06 10:30:36 +0100

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="bg-secondary flex flex-col justify-center h-screen items-center font-bold text-black text-center">
+    <div className="bg-primary flex flex-col justify-center h-screen items-center font-bold text-white text-center">
       <h1 className="text-5xl font-montserrat">404 | PAGE NOT FOUND</h1>
       <p className="py-6 text-xl">
         Sorry, the page you requested cannot be found, Please navigate back to
