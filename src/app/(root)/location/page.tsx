e910a2fe81cb5ee0105778890a59f984dac219fa
theheemmanuel <oladirane@gmail.com>
2025-07-11 00:56:46 +0100

    fix

--- src/app/(root)/location/page.tsx
+++ src/app/(root)/location/page.tsx
@@ -2,32 +2,20 @@ import { Metadata, NextPage } from "next";
 import React from "react";
 import CurrentLocation from "@/components/Location/CurrentLocation";
 
-export async function generateMetadata({
-  searchParams,
-}: {
-  searchParams: { id?: string };
-}): Promise<Metadata> {
-  const locationId = searchParams.id;
-
-  return {
-    title: locationId
-      ? `${locationId} | Redeemer's University`
-      : "Location | Redeemer's University",
+export const metadata: Metadata = {
+  title: "Location | Redeemer's University",
+  description:
+    "An interactive campus navigation app for Redeemer's University, helping visitors and students easily find their way around campus with maps, location scanning, and virtual video tours",
+  openGraph: {
+    title: "Location | Redeemer's University",
     description:
       "An interactive campus navigation app for Redeemer's University, helping visitors and students easily find their way around campus with maps, location scanning, and virtual video tours",
-    openGraph: {
-      title: locationId
-        ? `${locationId} | Redeemer's University`
-        : "Location | Redeemer's University",
-      description:
-        "An interactive campus navigation app for Redeemer's University, helping visitors and students easily find their way around campus with maps, location scanning, and virtual video tours",
-      images: [
-        new URL("/gate.jpg", process.env.NEXT_PUBLIC_BASE_URL as string).href,
-      ],
-      type: "website",
-    },
-  };
-}
+    images: [
+      new URL("/gate.jpg", process.env.NEXT_PUBLIC_BASE_URL as string).href,
+    ],
+    type: "website",
+  },
+};
 
 const LocationPage: NextPage = () => {
   return (
